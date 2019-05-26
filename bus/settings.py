ab9f3141366dfc31ed21abc68ac33a408cda43b3
Taekho Nam <career.thnam@gmail.com>
2019-05-26 22:06:55 +0900

    settings.py에 static file path 설정

--- bus/settings.py
+++ bus/settings.py
@@ -108,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [  # FileSystem Static Finder에 의해서 사용되는 설정
+    ROOT('bus', 'static'),
+]
